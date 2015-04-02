77167d2bf2d7364209a7763abcdca22795cd1ed9
Keith Benedict <keith@benedicthome.com>
2015-04-02 10:29:17 -0400

    added vip-roles.php dependency

--- read-only-role.php
+++ read-only-role.php
@@ -8,6 +8,8 @@ Author: Keith Benedict
 License: MIT
 */
 
+require_once ABSPATH . 'wp-content/themes/vip/plugins/vip-do-not-include-on-wpcom/vip-roles.php';
+
 add_action( 'init', 'mehtryx_custom_readonly_role' );
 function mehtryx_custom_readonly_role() {
     wpcom_vip_add_role( 'readonly', 'Read Only', array( 'read' => true, ) );
